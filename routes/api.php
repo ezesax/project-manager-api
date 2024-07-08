cd2fcc81cf67eaed30596b2411b484c1e8743fa7
ezesax <ezequieldavidromano@hotmail.com>
2024-07-08 10:49:46 -0300

    Cleaning the routes

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('App\Http\Controllers\Api')->group(function (){
-    Route::post('/users', 'UserController@store');
-    Route::post('/login', 'AuthController@login');
+    Route::post('users', 'UserController@store');
+    Route::post('login', 'AuthController@login');
 
     Route::middleware('auth.jwt')->group(function () {
-        Route::apiResource('/users', 'UserController')->except(['store']);
-        Route::apiResource('/projects', 'ProjectController');
-        Route::apiResource('/tasks', 'TaskController');
-        Route::apiResource('/comments', 'CommentController');
-        Route::apiResource('/tags', 'TagController');
+        Route::apiResource('users', UserController::class)->except(['store']);
+        Route::apiResource('projects', ProjectController::class);
+        Route::apiResource('tasks', TaskController::class);
+        Route::apiResource('comments', CommentController::class);
+        Route::apiResource('tags', TagController::class);
 
-        Route::post('/logout', 'AuthController@logout');
-        Route::get('/me', 'AuthController@me');
+        Route::post('logout', 'AuthController@logout');
+        Route::get('me', 'AuthController@me');
     });
 });
