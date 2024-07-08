3d0ae0eaed3891314de1f0fd5c32d5c410f3ef8c
ezesax <ezequieldavidromano@hotmail.com>
2024-07-08 10:29:05 -0300

    Adding route namespace

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/users', 'App\Http\Controllers\Api\UserController@store');
-Route::post('/login', 'App\Http\Controllers\Api\AuthController@login');
+Route::namespace('App\Http\Controllers\Api')->group(function (){
+    Route::post('/users', 'UserController@store');
+    Route::post('/login', 'AuthController@login');
 
-Route::middleware('auth.jwt')->group(function () {
-    Route::apiResource('/users', 'App\Http\Controllers\Api\UserController')->except(['store']);
-    Route::apiResource('/projects', 'App\Http\Controllers\Api\ProjectController');
-    Route::apiResource('/tasks', 'App\Http\Controllers\Api\TaskController');
-    Route::apiResource('/comments', 'App\Http\Controllers\Api\CommentController');
-    Route::apiResource('/tags', 'App\Http\Controllers\Api\TagController');
+    Route::middleware('auth.jwt')->group(function () {
+        Route::apiResource('/users', 'UserController')->except(['store']);
+        Route::apiResource('/projects', 'ProjectController');
+        Route::apiResource('/tasks', 'TaskController');
+        Route::apiResource('/comments', 'CommentController');
+        Route::apiResource('/tags', 'TagController');
 
-    Route::post('/logout', 'App\Http\Controllers\Api\AuthController@logout');
-    Route::get('/me', 'App\Http\Controllers\Api\AuthController@me');
+        Route::post('/logout', 'AuthController@logout');
+        Route::get('/me', 'AuthController@me');
+    });
 });
